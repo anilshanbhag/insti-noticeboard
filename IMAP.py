f4c6f96b87e218e2518385d812c9795748c3334d
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2012-08-20 01:08:57 +0530

    changes in IMAP

--- IMAP.py
+++ IMAP.py
@@ -1,9 +1,12 @@
-import imaplib, getpass
+import imaplib, getpass, email
 
 
 class IMAPWrapper:
-    """Class to connect to imap server login and extracting emails between two given dates"""
-    mail=None
+    """
+    Class to connect to imap server login and extracting emails 
+    between two given dates
+    """
+    
     def __init__(self,username,password):
         try:
             self.mail=imaplib.IMAP4('imap.iitb.ac.in',143)
@@ -16,16 +19,22 @@ class IMAPWrapper:
         query = '(SINCE '+date1+' BEFORE '+date2+' )'
         typ,data=self.mail.search(None,query)
         for num in data[0].split():
-            head=self.mail.fetch(num,'(RFC822.HEADER)')[1][0]
-            msg=self.mail.fetch(num,'(RFC822.TEXT)')[1][0]
-            msgs.append([head,msg])
+            msg=self.mail.fetch(num,'(RFC822)')
+            msg=email.message_from_string(msg[1][0][1])
+            body=""
+            for part in msg.walk():
+            	if part.get_content_type() == 'text/plain':
+            		body="\n" + part.get_payload() + "\n"
+            msgs.append([msg['From'],msg['Subject'],body])
         return msgs
 
 if __name__=="__main__":
-    username=raw_input("Username: ")
-    password=getpass.getpass()
+    #username=raw_input("Username: ")
+    #password=getpass.getpass()
+    username="110050039"
+    password="11061993"
     date1="14-Aug-2012"
     date2="15-Aug-2012"
     acc = IMAPWrapper(username,password)
     msgs=acc.get_mails(date1,date2)
-    print msgs[0]
+    print msgs[5]
